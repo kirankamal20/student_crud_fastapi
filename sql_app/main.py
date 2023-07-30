3009f9d9a0411b89edf1cf7b34f6f8aaa400815b
kirankamal <kiran.k@lucidplus.com>
2023-07-31 04:05:23 +0530

    code updated

--- sql_app/main.py
+++ sql_app/main.py
@@ -55,6 +55,9 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 @app.post("/users/{user_id}/students/", response_model=schemas.Student)
 async def add_student(
     user_id: int, student_name: Annotated[str, Form()], student_age: Annotated[str, Form()],date_of_birth: Annotated[str, Form()] ,gender: Annotated[str, Form()],country: Annotated[str, Form()], image:  UploadFile = File(...) , db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
     file_name = os.path.basename(image.filename)
     file_path = f"images/{file_name}"
 
